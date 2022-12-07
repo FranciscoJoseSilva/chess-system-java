3f6472803f29873a2537063d7622c3e40f5cf408
Francisco Jose <sousa06092001@gmail.com>
2022-12-07 12:41:33 -0400

    Starting to implement Board and Piece

---
+++ src/bordgame/Piece.java
@@ -0,0 +1,19 @@
+package bordgame;
+
+public abstract class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	
+	
+	
+}
